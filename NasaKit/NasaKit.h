05327504355137e77192d288b11088436c489b63
Johann Kerr <johannkerr@gmail.com>
2016-08-15 09:40:03 -0400

    Initial Commit

---
+++ NasaKit/NasaKit.h
@@ -0,0 +1,19 @@
+//
+//  NasaKit.h
+//  NasaKit
+//
+//  Created by Johann Kerr on 8/15/16.
+//  Copyright © 2016 Johann Kerr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NasaKit.
+FOUNDATION_EXPORT double NasaKitVersionNumber;
+
+//! Project version string for NasaKit.
+FOUNDATION_EXPORT const unsigned char NasaKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NasaKit/PublicHeader.h>
+
+
